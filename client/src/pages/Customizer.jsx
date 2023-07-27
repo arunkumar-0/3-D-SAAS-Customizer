1bb12b6ca9b420dd0142c395c8535adde79a88ab
ARUN_KUMAR <gurukularunyadav@gmail.com>
2023-07-27 18:13:46 +0530

    added new port to listen

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -55,7 +55,7 @@ const Customizer = () => {
       //call our backened for image 
       setGeneratingImg(true);
 
-      const response = await fetch ('http://localhost:8080/api/v1/dalle' , {
+      const response = await fetch ('http://localhost:10000/api/v1/dalle' , {
         method: 'POST',
         headers : {
           'Content-Type' : 'application/json'
